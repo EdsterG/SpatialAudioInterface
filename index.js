9bff12788f8067b37eb7620728eb430bdc25874d
EdsterG <eddie_g_89@hotmail.com>
2015-07-09 18:06:36 -0700

    Moved osc logic into dedicated functions to allow for a library independent o/c implementation. Added support for bundles on client side.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var osc = require("node-osc");
+var nodeOSC = require("node-osc");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
@@ -15,34 +15,66 @@ var recvHost = "localhost"; // Set IP of computer running Node.js
 var recvPort = 7770;
 
 // Setup to receive OSC messages from server
-var oscRecv = new osc.Server(recvPort, recvHost);
-oscRecv.on("message", function(msg_array, rinfo) {
-  // Forward received message to all clients
-  io.emit("message", msg_array); // This function sends msg_array to all connected clients
-  console.log("Recv:", msg_array, "From:", rinfo);
+var oscRecv = new nodeOSC.Server(recvPort, recvHost);
+function OSCrecv(osc_msg) {
+  // Build OSC object
+  var osc_obj;
+  if (osc_msg[0] == "#bundle") {
+    console.log("Recv:", osc_msg);
+    var elems = [];
+    for (var i=2; i<osc_msg.length; i++) {
+      var elem = { oscType : "message",
+                   address : osc_msg[i][0],
+                   args : osc_msg[i].slice(1,osc_msg[i].length),
+                 }
+      elems.push(elem);
+    }
+    osc_obj = { oscType : "bundle",
+                timetag : osc_msg[1],
+                elements : elems,
+              }
+  } else {
+    osc_obj = { oscType : "message",
+                address : osc_msg[0],
+                args : osc_msg.slice(1,osc_msg.length),
+              }
+  }
+
+  // Send osc_obj to all clients
+  io.emit("OSC message", osc_obj); // This function sends osc_obj to all connected clients
+  console.log("Recv:", JSON.stringify(osc_obj));
+}
+oscRecv.on("message", function(osc_msg, rinfo) {
+  OSCrecv(osc_msg)
 });
 
 // Setup to send OSC messages from clients
-var oscSend = new osc.Client(sendHost, sendPort);
+var oscSend = new nodeOSC.Client(sendHost, sendPort);
+function OSCsend(osc_obj, socket) {
+  // Build OSC message
+  var osc_msg =  new nodeOSC.Message(osc_obj.address);
+  for (var i=0; i < osc_obj.args.length; i++) {
+    osc_msg.append(osc_obj.args[i]);
+  }
+  // Send osc_msg to server
+  oscSend.send(osc_msg);
+  // Forward osc_obj to all other clients
+  socket.broadcast.emit("OSC message", osc_obj); // This function sends osc_obj to all connected clients, excluding the current client
+  console.log("Send:", JSON.stringify(osc_obj));
+}
 
 // Every time a client connects the following is executed:
 io.on("connection", function(socket){
-  socket.on("message", function (msg_array) {
-    // Build OSC message
-    var osc_msg =  new osc.Message(msg_array[0]);
-    for (var i=1; i < msg_array.length; i++) {
-      osc_msg.append(msg_array[i]);
-    }
-    // Forward osc_msg to server from current client
-    oscSend.send(osc_msg);
-    // Forward msg_array to other clients (excluding the current client)
-    socket.broadcast.emit("message", msg_array); 
-    console.log("Send:", msg_array);
+  // Setup osc forwarding from this client to the server
+  socket.on("OSC message", function(osc_obj) {
+    OSCsend(osc_obj, socket);
   });
-  
-  // oscSend.send("/status", socket.sessionId + " connected");
 
+  // OSCsend({address:"/status", args:socket.id+" connected" });
+  // console.log("client connected");
+  
   // socket.on("disconnect", function(){
+  //   OSCsend({address:"/status", args:socket.id+" disconnected" });  
   //   console.log("client disconnected");
   // });
 });
